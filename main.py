55ce5fd599096807380c98ed7b33728abf3f1c20
Sean Caspy <seancaspy@gmail.com>
2025-12-06 05:38:55 +0200

    new feature added, extract gameplay

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 from chessdotcom import get_player_games_by_month, Client
 import re
 
-def get_player_game(game_pgn, user_name):
-    pass
+def get_player_game(game_pgn):
+    """
+        returns string describes the moves and time of the game
+        Args:
+             game_pgn(str): all the valibals of the game
 
-def get_player_color(game_pgn):
+        """
+    results = re.findall(r'1\.\s*(.*?)(?=\d-\d)', game_pgn)
+    results = [f"{i + 1}. {move}" for i, move in enumerate(results)]
+    return results
+
+
+def get_player_color(game_pgn, userName):
     """
     Returns 'white', 'black', or None based on PGN and username
 
     Args:
         game_pgn (str): The PGN string of the game
-        username (str): Your username to find
 
     Returns:
         str: 'white', 'black', or None
     """
-    userName = "aui129"
     pattern = rf'\[(\w+)\s+"{re.escape(userName)}"\]'
     match = re.search(pattern, game_pgn)
 
     if match:
         return match.group(1).lower()
     return None
+
 
 
 Client.request_config["headers"]["User-Agent"] = ("MeChess/0.1 (Contact: Seancaspy@gmail.com)")
@@ -32,8 +40,6 @@ year = int(input("enter year: "))
 
 
 history = get_player_games_by_month(user_name, year, month)
-# print(history)
 for game in history.games:
-    print(f"you played as: {get_player_color(game.pgn)}")
-
+    print(f"you played as {get_player_color(game.pgn, user_name)} \n{get_player_game(game.pgn)}")
 
